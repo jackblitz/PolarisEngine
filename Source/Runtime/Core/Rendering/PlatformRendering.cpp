3ef439a631e8f5adb6c05a52cd4c707faa01b9af
heybuildthis <luke@heybuildthis.com>
2025-06-23 19:37:19 -0700

    SDL added as submodule

---
+++ Source/Runtime/Core/Rendering/PlatformRendering.cpp
@@ -0,0 +1,3 @@
+//
+// Created by luke on 6/23/2025.
+//
---
+++ Source/Runtime/Core/Rendering/PlatformRendering.h
@@ -0,0 +1,8 @@
+//
+// Created by luke on 6/23/2025.
+//
+
+#ifndef PLATFORMRENDERING_H
+#define PLATFORMRENDERING_H
+
+#endif //PLATFORMRENDERING_H
